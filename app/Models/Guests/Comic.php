e25573947e91ca38e2e1b3c8d98e2ca9e1ba829a
leonardoserra <serra.leonardo92@gmail.com>
2023-05-16 15:46:11 +0200

    creato layouts app con yields e include

---
+++ app/Models/Guests/Comic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Guests;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comic extends Model
+{
+    use HasFactory;
+}
